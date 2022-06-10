1670162ea338d617d2446358d6ece98e78e570d7
Nicko Sinnikov <nickosinnikov@outlook.com>
2022-06-10 22:06:33 +0300

    first commit

---
+++ ready-html/js/app.js
@@ -0,0 +1,36 @@
+// 3d-scroll
+
+let zSpacing = -1000, //отвечает за расстояние по оси Z
+  lastPos = zSpacing / 5,
+  $frames = document.getElementsByClassName('frame'),
+  frames = Array.from($frames),
+  zVals = []; //массив со значениями по оси Z
+
+window.onscroll = function () {
+  let top = document.documentElement.scrollTop,
+    delta = lastPos - top;
+
+  lastPos = top;
+
+  frames.forEach(function (n, i) {
+    zVals.push(i * zSpacing + zSpacing);
+    zVals[i] += delta * -5;
+    let frame = frames[i],
+      transform = `translateZ(${zVals[i]}px)`,
+      opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
+
+    frame.setAttribute('style', `transform:${transform}; opacity: ${opacity}`);
+  });
+};
+
+window.scrollTo(0, 1);
+
+//audio
+
+let soundButton = document.querySelector('.soundbutton'),
+  audio = document.querySelector('.audio');
+
+soundButton.addEventListener('click', (e) => {
+  soundButton.classList.toggle('paused');
+  audio.paused ? audio.play() : audio.pause();
+});
